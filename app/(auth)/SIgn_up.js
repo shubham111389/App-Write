f4d50797788710623c43740fc5688b46f5c97d12
shubham annpurne <shubhamannpurne1029@gmail.com>
2024-07-19 00:44:11 +0530

    create a folder structure for the auth functionality

---
+++ app/(auth)/SIgn_up.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Sign_up = () => {
+  return (
+    <View>
+      <Text>Sign_up</Text>
+    </View>
+  )
+}
+
+export default Sign_up
---
+++ app/(auth)/Sign_in.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Sign_in = () => {
+  return (
+    <View>
+      <Text>Sign_in</Text>
+    </View>
+  )
+}
+
+export default Sign_in
